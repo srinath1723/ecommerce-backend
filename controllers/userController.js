26fa209b73d3e499933f01f39fb77ed71dd731d0
srinath1723 <srinathsp890.com>
2024-07-07 21:29:25 +0530

    crud --users endpoints done

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,6 @@ const User = require("../models/user");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../utils/config');
-
 const userController = {
     // getData: (req, res) => {
     //     res.send('Hello World!!');
@@ -11,24 +10,18 @@ const userController = {
         try {
             // get the user inputs from the request body
             const { name, email, password } = req.body;
-
             // check if the user already exists in the database
             const user = await User.findOne({ email });
-
             // if the user already exists, return an error
             if (user) {
                 return res.send({ message: 'User already exists' });
             }
-
             // hash the password
             const hashedPassword = await bcrypt.hash(password, 10);
-
             // create a new user
             const newUser = new User({ name, email, password: hashedPassword });
-
             // save the user to the database
             const savedUser = await newUser.save();
-
             // return the saved user
             res.send({ message: 'User created successfully', user: savedUser });
         } catch (error) {
@@ -39,26 +32,20 @@ const userController = {
         try {
             // get the user inputs from the request body
             const { email, password } = req.body;
-
             // check if the user exists in the database
             const user = await User.findOne({ email });
-
             // if the user does not exist, return an error
             if (!user) {
                 return res.send({ message: 'User does not exist' });
             }
-
             // check if the password is correct
             const isPasswordCorrect = await bcrypt.compare(password, user.password);
-
             // if the password is incorrect, return an error
             if (!isPasswordCorrect) {
                 return res.send({ message: 'Invalid credentials' });
             }
-
             // create a token
             const token = jwt.sign({ id: user._id }, JWT_SECRET);
-
             //  set a cookie with the token
             res.cookie('token', token, {
                 httpOnly: true,
@@ -66,10 +53,8 @@ const userController = {
                 secure: true,
                 expires: new Date(new Date().getTime() + 24 * 60 * 60 * 1000)  // 24 hours from now
             });
-
             // return the user
             res.send({ message: 'Login successful' });
-
         } catch (error) {
             res.send({ message: error.message })
         }
@@ -77,34 +62,86 @@ const userController = {
 
     logout: async (req, res) => {
         try {
+            const userId = req.userId;
+
+            if (!userId) {
+                return res.send({ message: 'User not logged in' });
+            }
+
             // clear the cookie
             res.clearCookie('token');
 
             // return the user
             res.send({ message: 'Logout successful' });
+        } catch (error) {
+            res.send({ message: error.message })
+        }
+    },
+    // get the user profile
+    getProfile: async (req, res) => {
+        try {
+            // get the user id from the request object
+            const userId = req.userId;
+            // find the user by id
+            const userProfile = await User.findById(userId);
+            // if the user does not exist, return an error
+            if (!userProfile) {
+                return res.send({ message: 'User does not exist' });
+            }
+            // return the user profile
+            res.send({ message: 'User profile', user: userProfile });
 
         } catch (error) {
             res.send({ message: error.message })
         }
     },
 
-    // get the user profile
-    getProfile: async (req, res) => {
+    updateProfile: async (req, res) => {
         try {
             // get the user id from the request object
             const userId = req.userId;
 
+            // get the user inputs from the request body
+            const { name, email } = req.body;
 
             // find the user by id
-            const userProfile = await User.findById(userId);
+            const user = await User.findById(userId);
 
             // if the user does not exist, return an error
-            if (!userProfile) {
+            if (!user) {
                 return res.send({ message: 'User does not exist' });
             }
 
-            // return the user profile
-            res.send({ message: 'User profile', user: userProfile });
+            // update the user profile
+            user.name = name || user.name;
+            user.email = email || user.email;
+
+            // save the user to the database
+            const updatedUser = await user.save();
+
+            // return the updated user profile
+            res.send({ message: 'User profile updated successfully', user: updatedUser });
+
+        } catch (error) {
+            res.send({ message: error.message })
+        }
+    },
+
+    deleteProfile: async (req, res) => {
+        try {
+            // get the user id from the request object
+            const userId = req.userId;
+
+            // find the user by id and delete
+            const deletedUser = await User.findByIdAndDelete(userId);
+
+            // if the user does not exist, return an error
+            if (!deletedUser) {
+                return res.send({ message: 'User does not exist' });
+            }
+
+            // return the deleted user
+            res.send({ message: 'User deleted successfully', user: deletedUser });
 
         } catch (error) {
             res.send({ message: error.message })
@@ -112,5 +149,4 @@ const userController = {
     }
     
 }
-
 module.exports = userController;
